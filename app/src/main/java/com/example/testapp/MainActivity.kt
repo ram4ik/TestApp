8030247c0b954bb425cea1771dfb02bef2074dca
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-01-03 10:28:19 +0200

    Initial commit

---
+++ app/src/main/java/com/example/testapp/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.testapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var count = 0
+
+        btn.setOnClickListener(){
+            txt.text = "$count"
+            count += 1
+            txt.text = "$count"
+        }
+    }
+}
